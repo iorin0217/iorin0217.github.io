4218b6b4de1bb1fecd496d6c4f38b3d6a86a78e6
iorin0217 <ikuratan217@gmail.com>
2019-11-22 20:38:12 +0900

    戻した

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -76,8 +76,7 @@ const Main = props => {
 
   return (
     <main className={classes.main}>
-      {/*<SpringScrollbars>{children}</SpringScrollbars>*/}
-      {children}
+      <SpringScrollbars>{children}</SpringScrollbars>
     </main>
   );
 };
